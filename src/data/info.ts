74aab27f4925f537f4e4415ccdcb1e13e3a4baea
Syed Uzair <80479109+Youknowzair@users.noreply.github.com>
2022-11-30 00:56:12 +0530

    Update info.ts

--- src/data/info.ts
+++ src/data/info.ts
@@ -38,7 +38,7 @@ export const info = {
       isFeatured: true,
       thumbnail: "/assets/images/astro-portfolio.png",
       githubUrl: "https://github.com/Youknowzair/astro-portfolio10",
-      liveUrl: "https://uzairsyed.vercel.app/",
+      liveUrl: "https://uzairsyed.netlify.app/",
     },
   ],
   
